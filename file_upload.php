04a1149483ab987df7331224adf3669b268fe3c1
Hetalben Vaghela <hetalvaghela106@gmail.com>
2021-08-18 06:38:09 +0530

    add s3

--- file_upload.php
+++ file_upload.php
@@ -12,43 +12,19 @@ Select image :
 
 </form>
 <?php
- // header("Content-Type: text/html; charset=utf-8");
-use Aws\S3\S3Client;
-use Aws\S3\Exception\S3Exception;
+  header("Content-Type: text/html; charset=utf-8");
+
  if(isset($_POST['Submit1']))
 { 
 $file_tmp = $_FILES['file']['tmp_name'];
-$file_name=$_FILES["file"]["name"];
-$filepath = "img/" . $_FILES["file"]["name"];
- // Instantiate an Amazon S3 client.
-        $s3 = new S3Client([
-            'version' => 'latest',
-            'region'  => 'us-east-1',
-            'credentials' => [
-                'key'    => "AKIAZU35UPY4HX3FWDHS",
-                'secret' => "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",
-            ],
-        ]);
-        try {
-            $result = $s3->putObject([
-                'Bucket' => 'phpupload1',
-                'Key'    => $file_name,
-                'SourceFile' => $file_tmp,
-                 'ACL'       => 'public-read',
+$fnm = $_FILES["file"]["name"];
+$filepath = "uploads/" . $_FILES["file"]["name"];
 
-            ]);
-            var_dump($result);
-        } 
-		catch (S3Exception $e) {
-    // Catch an S3 specific exception.
-    echo $e->getMessage();
-}
 $up=move_uploaded_file($file_tmp, $filepath);
-  copy($file_tmp, $filepath);
-
 
 if($up) 
-{
+{	
+
 echo "<img src=".$filepath." height=200 width=300 />";
 } 
 else 
@@ -59,4 +35,4 @@ echo "Error !!";
 ?>
 
 </body>
-</html
+</html>
